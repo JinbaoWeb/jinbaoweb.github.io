40a72d3bd7711d26cfeb93461aa1a665ba0c7fdd
JinbaoWeb <jinbaosite@yeah.net>
2025-09-15 20:19:27 +0800

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,14 @@
+import Navbar from "./Navbar";
+
 export default function Layout({ children }) {
   return (
-    <div className="bg-gray-50 min-h-screen flex justify-center px-6 py-10">
-      <article className="prose prose-blue lg:prose-lg bg-white rounded-2xl shadow p-10 max-w-4xl w-full">
-        {children}
-      </article>
-    </div>
+    <>
+      <Navbar />
+      <div className="bg-gray-50 min-h-screen flex justify-center px-6 py-24">
+        <article className="prose prose-blue lg:prose-lg bg-white rounded-2xl shadow p-10 max-w-4xl w-full">
+          {children}
+        </article>
+      </div>
+    </>
   );
 }
