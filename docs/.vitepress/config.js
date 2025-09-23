c8ba93d550db406a143583479087d49865b85f9c
JinbaoWeb <jinbaosite@yeah.net>
2025-09-23 11:59:35 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: "JinbaoWeb",
+  description: "基于 VitePress 构建",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "关于", link: "/" }
+    ],
+    sidebar: []
+  }
+}
