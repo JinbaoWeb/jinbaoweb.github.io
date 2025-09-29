9d3d5f6a387b76efff242922ff3dc844c2d7d7a3
JinbaoWeb <jinbaosite@yeah.net>
2025-09-29 17:05:57 +0800

    Delete docs/.vitepress/theme directory

--- docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-// docs/.vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
-import MyLayout from './Layout.vue'
-
-export default {
-  ...DefaultTheme,
-  // 覆盖默认 Layout
-  Layout: MyLayout
-}
