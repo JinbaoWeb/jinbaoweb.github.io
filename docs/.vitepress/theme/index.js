8da8bc36e2c3ede145e4f68f10bdb88376c33163
JinbaoWeb <jinbaosite@yeah.net>
2026-01-20 12:11:17 +0800

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import CategoryPage from './CategoryPage.vue'
+import ListPage from './ListPage.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component('CategoryPage', CategoryPage)
+    app.component('list', ListPage)
   }
 }
