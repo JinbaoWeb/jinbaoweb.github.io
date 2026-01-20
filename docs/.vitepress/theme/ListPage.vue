52de1628f883844335fb68638d7428903dadd244
JinbaoWeb <jinbaosite@yeah.net>
2026-01-20 12:10:45 +0800

    Create ListPage.vue

---
+++ docs/.vitepress/theme/ListPage.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="blog-timeline">
+    
+    <section 
+      v-for="topic in topics" 
+      :key="topic.title"
+      class="topic"
+    >
+      <div class="topic-title">{{ topic.title }}</div>
+      <div class="article-list">
+        
+        <div 
+          v-for="article in topic.articles" 
+          :key="article.link"
+          class="article"
+        >
+          <div class="article-meta">{{ article.date }}</div>
+          <div class="article-title">
+            <a :href="article.link">{{ article.title }}</a>
+          </div>
+        </div>
+        
+      </div>
+    </section>
+    
+  </div>
+</template>
+
+<script setup>
+import { useData } from 'vitepress'
+
+const { frontmatter } = useData()
+const topics = frontmatter.value.topics || []
+</script>
+
+<style>
+    .topic {
+      margin-bottom: 50px;
+      max-width: 800px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .topic-title {
+      font-size: 1.2em;
+      font-weight: bold;
+      color: #007bff;
+      border-left: 6px solid #007bff;
+      padding-left: 10px;
+      margin-bottom: 20px;
+    }
+
+    .article-list {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+    }
+
+    .article {
+      background: #fff;
+      border-radius: 10px;
+      padding: 14px 20px;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+      transition: 0.2s ease;
+    }
+
+    /* .article:hover {
+      transform: translateY(-3px);
+      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+    } */
+
+    .article-title {
+      font-size: 1em;
+      font-weight: 600;
+      color: #333;
+      margin-bottom: 4px;
+    }
+
+    .article-title a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    .article-title a:hover {
+      color: #007bff;
+    }
+
+    .article-meta {
+      font-size: 0.9em;
+      color: #777;
+    }
+
+    @media (max-width: 600px) {
+      body {
+        padding: 20px;
+      }
+      .topic-title {
+        font-size: 1.3em;
+      }
+    }
+</style>
